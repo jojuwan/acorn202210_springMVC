0984f98e682e5d406e083d50b0cca9c64d936c7d
jojuwan <whwndhks123@naver.com>
2022-12-21 13:11:00 +0900

    Spring04 에 프로필 이미지 수정 기능 추가함

--- Spring04_Final/src/main/java/com/gura/spring04/users/service/UsersServiceImpl.java
+++ Spring04_Final/src/main/java/com/gura/spring04/users/service/UsersServiceImpl.java
@@ -1,5 +1,7 @@
 package com.gura.spring04.users.service;
 
+import java.io.File;
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -17,7 +19,7 @@ import com.gura.spring04.users.dto.UsersDto;
 
 @Service
 public class UsersServiceImpl implements UsersService{
-
+	
 	@Autowired
 	private UsersDao dao;
 	
@@ -31,13 +33,13 @@ public class UsersServiceImpl implements UsersService{
 	public void addUser(UsersDto dto) {
 		//가입시 입력한 비밀번호를 읽어와서
 		String pwd=dto.getPwd();
-		//암호화 한후에
+		//암호화 한후에 
 		BCryptPasswordEncoder encoder=new BCryptPasswordEncoder();
 		String encodedPwd=encoder.encode(pwd);
 		//dto 에 다시 넣어준다.
 		dto.setPwd(encodedPwd);
 		//암호화된 비밀번호가 들어 있는 dto 를 dao 에 전달해서 새로운 회원 정보를 추가 한다.
-		dao.insert(dto);		
+		dao.insert(dto);
 	}
 	//로그인 처리를 하는 메소드
 	@Override
@@ -45,14 +47,14 @@ public class UsersServiceImpl implements UsersService{
 		//입력한 정보가 맞는지 여부
 		boolean isValid=false;
 		//아이디를 이용해서 회원 정보를 얻어온다.
-		UsersDto resultDto=dao.getData(dto.getId());		
-		//만일 select 된 회원 정보가 존재하고
+		UsersDto resultDto=dao.getData(dto.getId());
+		//만일 select 된 회원 정보가 존재하고 
 		if(resultDto != null) {
-			//BCrypt 클래스의 static 메소드를 이용해서 입력한 비밀번호와 암호화 해서 저장된 비밀번호 일치 여부를 알아내야한다.
+			//Bcrypt 클래스의 static 메소드를 이용해서 입력한 비밀번호와 암호화 해서 저장된 비밀번호 일치 여부를 알아내야한다.
 			isValid = BCrypt.checkpw(dto.getPwd(), resultDto.getPwd());
 		}
 		
-		//만일 유효한 정보이면
+		//만일 유효한 정보이면 
 		if(isValid) {
 			//로그인 처리를 한다.
 			session.setAttribute("id", resultDto.getId());
@@ -61,13 +63,12 @@ public class UsersServiceImpl implements UsersService{
 
 	@Override
 	public void getInfo(HttpSession session, ModelAndView mView) {
-		//로그인된 아이디를 읽어온다.
+		//로그인된 아이디를 읽어온다. 
 		String id=(String)session.getAttribute("id");
-		//DB 에서 회원 정보를 얻어와서
+		//DB 에서 회원 정보를 얻어와서 
 		UsersDto dto=dao.getData(id);
 		//ModelAndView 객체에 담아준다.
 		mView.addObject("dto", dto);
-		
 	}
 
 	@Override
@@ -99,32 +100,59 @@ public class UsersServiceImpl implements UsersService{
 		//작업의 성공여부를 ModelAndView 객체에 담아 놓는다(결국 HttpServletRequest 에 담긴다)
 		mView.addObject("isSuccess", isValid);
 		//로그인된 아이디도 담아준다.
-		mView.addObject("id", id);	
+		mView.addObject("id", id);		
 	}
 
 	@Override
 	public Map<String, Object> saveProfileImage(HttpServletRequest request, MultipartFile mFile) {
-		// TODO Auto-generated method stub
-		return null;
+		//업로드된 파일에 대한 정보를 MultipartFile 객체를 이용해서 얻어낼수 있다.	
+		
+		//원본 파일명
+		String orgFileName=mFile.getOriginalFilename();
+		//upload 폴더에 저장할 파일명을 직접구성한다.
+		// 1234123424343xxx.jpg
+		String saveFileName=System.currentTimeMillis()+orgFileName;
+		
+		// webapp/upload 폴더까지의 실제 경로 얻어내기 
+		String realPath=request.getServletContext().getRealPath("/resources/upload");
+		// upload 폴더가 존재하지 않을경우 만들기 위한 File 객체 생성
+		File upload=new File(realPath);
+		if(!upload.exists()) {//만일 존재 하지 않으면
+			upload.mkdir(); //만들어준다.
+		}
+		try {
+			//파일을 저장할 전체 경로를 구성한다.  
+			String savePath=realPath+File.separator+saveFileName;
+			//임시폴더에 업로드된 파일을 원하는 파일을 저장할 경로에 전송한다.
+			mFile.transferTo(new File(savePath));
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		// json 문자열을 출력하기 위한 Map 객체 생성하고 정보 담기 
+		Map<String, Object> map=new HashMap<String, Object>();
+		map.put("imagePath", "/resources/upload/"+saveFileName);
+		
+		return map;
 	}
 
 	@Override
 	public void updateUser(UsersDto dto, HttpSession session) {
 		//수정할 회원의 아이디
-	    String id=(String)session.getAttribute("id");
-	    //dto 에 id 도 넣어준다. 
-	    dto.setId(id);
-	    //만일 프로필 이미지를 등록하지 않은 상태이면
-	    if(dto.getProfile().equals("empty")) {
-	       //users 테이블의  profile 칼럼을 null 인 상태로 유지하기 위해 profile 에 null 을 넣어준다.
-	       dto.setProfile(null);
-	    }
-	    dao.update(dto);		
+		String id=(String)session.getAttribute("id");
+		//dto 에 id 도 넣어준다. 
+		dto.setId(id);
+		//만일 프로필 이미지를 등록하지 않은 상태이면
+		if(dto.getProfile().equals("empty")) {
+			//users 테이블의  profile 칼럼을 null 인 상태로 유지하기 위해 profile 에 null 을 넣어준다.
+			dto.setProfile(null);
+		}
+		dao.update(dto);
 	}
 
 	@Override
 	public void deleteUser(HttpSession session, ModelAndView mView) {
-		//로그인된 아이디를 얻어와서
+		//로그인된 아이디를 얻어와서 
 		String id=(String)session.getAttribute("id");
 		//해당 정보를 DB 에서 삭제하고
 		dao.delete(id);
@@ -136,3 +164,7 @@ public class UsersServiceImpl implements UsersService{
 	}
 
 }
+
+
+
+
