c679bfb3728c858cb9cef963438c91652964cd61
jojuwan <whwndhks123@naver.com>
2023-01-02 11:06:00 +0900

    Boot07_Final 프로젝트 추가하고 기본 구성함

---
+++ Spring07_Final/src/main/java/com/gura/spring07/Spring07FinalApplication.java
@@ -0,0 +1,13 @@
+package com.gura.spring07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring07FinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring07FinalApplication.class, args);
+	}
+
+}
---
+++ Spring07_Final/src/test/java/com/gura/spring07/Spring07FinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gura.spring07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring07FinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
